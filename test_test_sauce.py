14b2c5c2c920ec0ca3f3fb09721d69631a0324d5
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-23 12:34:34 +0300

    Tarayıcının ekran görüntüsünü tarihe, kullanıcı adı ve şifreye göre kaydetme işlemini yaptım.

--- test_test_sauce.py
+++ test_test_sauce.py
@@ -7,31 +7,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import pytest
+from pathlib import Path
+from datetime import date
 
 class TestTest_Sauce(TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://www.saucedemo.com")
+        # Bugünün tarhini al. Bu tarih adında bir klasör var mı onu bir kontrol et. Yoksa o tarih isminde yeni bir klasör oluştur.
+        self.folderPath = str(date.today())
+        Path(self.folderPath).mkdir(exist_ok=True)
+
 
     def tearDown(self):
         self.driver.quit()
 
-    @pytest.mark.skip("username", "password", [("1", "1"),("kullaniciadim", "sifrem")])
+    @pytest.mark.parameterize("username", "password", [("1", "1"),("kullaniciadim", "sifrem")])
     def test_invalid_login(self, username, password):
         self.driver.get("https://www.saucedemo.com/")
-        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located((By.ID, "user-name")))
+        self.waitForElementVisible((By.ID, "user-name"))
         usernameInput = self.driver.find_element(By.ID, "user-name")
-        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located((By.ID, "password")))
+        self.waitForElementVisible((By.ID, "password"))
         passwordInput = self.driver.find_element(By.ID, "password")
         usernameInput.send_keys("1")
         passwordInput.send_keys("1")
         loginButton = self.driver.find_element(By.XPATH, "//*[@id='login-button']")
         loginButton.click()
         errorMessage = self.driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
+        self.driver.save_screenshot(f"{self.folderPath}/test-invalid-login-{username}-{password}.png")
         print(errorMessage)
         assert errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
         sleep(2)
+    def waitForElementVisible(self, locator, timeout=5):
+        WebDriverWait(self.driver, timeout).until(expected_conditions.visibility_of_element_located(locator))
+
 
     def test_valid_login(self):
         WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located((By.ID, "user-name")))
